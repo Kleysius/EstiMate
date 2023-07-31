0b4d3ef6f2f9a14518444312c03d71a8b6a6b34b
Kleysius <sebasti.thomass@gmail.com>
2023-07-31 16:37:56 +0200

    comment home.js

--- public/js/home.js
+++ public/js/home.js
@@ -55,20 +55,29 @@ window.addEventListener("load", () => {
     }
 
     const colorPalette = new ColorPalette();
- 
-    document.querySelector(".overlay-button-colors").addEventListener("click", () => {
-            colorPalette.setColors();
-            colorPalette.setCustomProperties();
-        });
 
+    document.querySelector(".overlay-button-colors").addEventListener("click", () => {
+        colorPalette.setColors();
+        colorPalette.setCustomProperties();
+    });
+
+
+    // Mur de logo
     const logoWall = document.querySelector(".logo-wall");
     const logo = logoWall.querySelector("img");
+    // Définir le nombre de répétitions du logo le long de l'axe horizontal (à gauche et à droite)
     const numCopiesX = 30;
+
+    // Définir le nombre de répétitions du logo le long de l'axe vertical (en haut et en bas)
     const numCopiesY = 30;
+
+    // Définir la distance maximale que le mur de logo peut se déplacer horizontalement en réponse aux mouvements de la souris
     const maxTranslateX = 10;
+
+    // Définir la distance maximale que le mur de logo peut se déplacer verticalement en réponse aux mouvements de la souris
     const maxTranslateY = 10;
 
-    // Créez des copies du logo et ajoutez-les au mur de logo
+    // Créer des copies du logo et les ajouter au mur de logo pour obtenir l'effet de répétition
     for (let y = 0; y < numCopiesY; y++) {
         for (let x = 0; x < numCopiesX; x++) {
             const copy = logo.cloneNode(true);
@@ -76,14 +85,30 @@ window.addEventListener("load", () => {
         }
     }
 
+    // Fonction qui met à jour la position du mur de logo en réponse aux mouvements de la souris
     function updateLogoWallPosition(event) {
+        // Obtenir les coordonnées horizontales (clientX) et verticales (clientY) de la position de la souris
         const { clientX, clientY } = event;
+
+        // Obtenir la largeur de la fenêtre du navigateur
         const windowWidth = window.innerWidth;
+
+        // Obtenir la hauteur de la fenêtre du navigateur
         const windowHeight = window.innerHeight;
+
+        // Normaliser les coordonnées horizontales de la souris pour les convertir en valeurs entre -1 et 1
         const normalizedX = (clientX / windowWidth) * 2 - 1;
+
+        // Normaliser les coordonnées verticales de la souris pour les convertir en valeurs entre -1 et 1
         const normalizedY = (clientY / windowHeight) * 2 - 1;
+
+        // Calculer le déplacement horizontal du mur de logo en fonction du mouvement de la souris
         const translateX = maxTranslateX * normalizedX;
+
+        // Calculer le déplacement vertical du mur de logo en fonction du mouvement de la souris
         const translateY = maxTranslateY * normalizedY;
+
+        // Appliquer les déplacements calculés au mur de logo en modifiant la propriété CSS "transform"
         logoWall.style.transform = `translate(${translateX}px, ${translateY}px)`;
     }
 
